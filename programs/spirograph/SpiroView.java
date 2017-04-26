3429d898a445a4eb2b9e03e64fc74dd1aa289dec
takahashi tomoki <tom_aaa_0905@yahoo.co.jp>
2017-04-26 22:51:39 +0900

    Add draw circle

--- programs/spirograph/SpiroView.java
+++ programs/spirograph/SpiroView.java
@@ -1,5 +1,8 @@
 package spirograph;
 
+import java.awt.Graphics;
+import java.awt.Dimension;
+
 public class SpiroView extends View
 {
   public SpiroView(SpiroModel aSpiroModel)
@@ -8,4 +11,19 @@ public class SpiroView extends View
     return;
   }
 
+  @Override
+  public void paintComponent(Graphics aGraphics)
+  {
+    SpiroModel spiroModel = (SpiroModel)model;
+    this.drawCircle(aGraphics,SpiroConstruct.SPIRO_WINDOW.width/2,SpiroConstruct.SPIRO_WINDOW.width/2,SpiroConstruct.SPIRO_WINDOW.width/2);
+    return;
+  }
+
+
+  private void drawCircle(Graphics aGraphics,Integer x,Integer y,Integer radius)
+  {
+    x = x - radius;
+    y = y - radius;
+    aGraphics.drawOval(x,y,radius*2,radius*2);
+  }
 }
